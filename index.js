4ba01cb0d403e4a2021f52973e7d5be19d7ff61d
fm-roshan <roshan.patil@flairminds.com>
2025-03-11 09:50:41 +0530

    added new product in the products page and testing google analytics

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://store.flairminds.com",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
     optionsSuccessStatus: 200,
